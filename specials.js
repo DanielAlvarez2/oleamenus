c21f288b4e801f61f117160fe611b9205ece0b83
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:14:56 -0400

    create specials.js

---
+++ specials.js
@@ -0,0 +1,157 @@
+
+let mushroomVeloute = `<span class="name">wild mushroom velout&eacute;</span><span class="description"> Manchego cheese foam, truffle zest, mushroom toast <strong>16</strong><br><span class="allergies">GARLIC, CHICKEN STOCK, DAIRY, gluten</span></span>`;
+
+let winterSalad = `
+            <span class="name">organic spring salad</span><br>
+            <span class="description"> 
+            mixed lettuce, rainbow beet, strawberry, tomato confit, 
+            fennel, caramelized walnuts, lotus chips, blood orange vinaigrette
+            <strong>16</strong><br>
+            <span class="allergies">nuts, can be made vegan</span></span>  
+
+`;
+
+let friedOysters = `
+<span class="name">fresh Chebooktook oysters</span><br>
+<span class="description"> 
+(Bouctouche Bay, New Brunswick, Canada), 
+Thai basil-black pepper mignonette sauce 
+and tomatillo granita <br>
+<strong>6 units 19 / 12 units 38</strong><br>
+<span class="allergies">SHELLFISH</span></span>`;
+
+let boquerones = `
+<span class="name">boquerones</span><br>
+<span class="description"> 
+marinated fresh anchovies, avocado-cilantro pur&eacute;e, toast 
+<strong>14</strong><br>
+<span class="allergies">gluten</span></span>`;
+
+let smokedSalmon = `
+<span class="name">smoked salmon</span><br>
+<span class="description"> 
+bulgur, mint, dill, parsley, lemon, crème fraîche, toast 
+<strong>17</strong><br>
+<span class="allergies">GARLIC, gluten</span>
+</span>`;
+
+
+let empanada = `
+<span class="name">cod fish empanada</span><br>
+<span class="description"> 
+galician cod fish pie, saffron, tomato, onion, pepper, 
+white miso a&iuml;oli, mixed greens 
+<strong>15</strong><br>
+<span class="allergies">GLUTEN, DAIRY</span></span>`;
+
+let brisket = `<span class="name">brisket</span><span class="description"> slow roasted; potatoes, mojo verde, mixed greens, japanese bbq sauce, aioli <strong>15</strong><br><span class="allergies">dairy, GARLIC, SOY/GLUTEN</span>`
+
+let mussels = `
+<span class="name">organic steamed PEI mussels</span>
+<span class="description"> 
+coconut milk, sweet piquillo pepper, lime, ginger, cilantro, garlic scapes
+<strong>18</strong><br>
+<span class="allergies">SHELLFISH, garlic</span></span>`
+
+let squashSoup = `
+<span class="name">creamy butternut squash soup</span><br>
+<span class="description"> 
+zucchini, roasted pumpkin seeds, brie cheese </span>
+<strong>14</strong><br>
+<span class="allergies">dairy, can be made vegan</span></span>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+let pasta = `<span class="name">squid ink pasta / seafood</span><br><span class="description">manila clams, shrimp, squid ink fresh pasta, fennel, tomato, carrots, parsley-white wine sauce </span> <strong>34</strong><br><span class="allergies">GLUTEN, GARLIC, SHELLFISH</span>`;
+
+let shortribs = `
+<span class="name">grass fed beef shortribs</span><br>
+<span class="description"> 
+boneless, braised, celery root purée, organic carrots, 
+Brussels sprouts, polenta cake, Rioja red wine-truffle jus
+<strong>38</strong><br>
+<span class="allergies">dairy</span></span>
+`;
+
+let cod = `
+<span class="name">fresh Atlantic cod gratin</span><br>
+<span class="description"> 
+aïoli, Spanish paprika-potato-cauliflower purée, 
+sun-dried tomatoes, black olives, scallions, 
+chorizo dust
+<strong>34</strong><br>
+<span class="allergies">GARLIC, pork</span></span>
+`;
+
+
+
+let wagyu = `
+<span class="name">Wagyu beef picanha coulotte (8oz)</span><br>
+<span class="description"> grass fed, potato confit, sweet piquillo peppers, ajilis-mojilis 
+<strong>49</strong><br>
+<span class="allergies">garlic</span></span>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let creamCheeseMousse = `
+<span class="name">caramel cystard flan & cream cheese mousse</span><br>
+<span class="description">
+dulce de leche sauce, pistachio tuile, crumbled almond cookie, 
+tahitian vanilla infused pineapple sauce, berries</span>
+<strong>13</strong><br>
+<span class="allergies">DAIRY, nuts</span></span>
+`;
+
+let honeycombBonbon = `
+<span class="name">honeycomb bonbon</span><br>
+<span class="description">
+70% dark chocolate, white chocolate-honey mousse, passion fruit curd, 
+honeycomb crumble, lavender ice cream
+<strong>14</strong><br>
+<span class="allergies">DAIRY, nuts</span></span>
+`;
+
+
+
+
+let flan = `
+<span class="name">caramel custard flan</span><br>
+<span class="description"> 
+lemon peel ice cream, dulce de leche sponge cake, pistachio tuile
+<strong>12</strong>
+<br>
+<span class="allergies">DAIRY, nuts, gluten</span></span>`;
+
+
+let praline = `
+            <span class="name">hazelnut praline dark chocolate mousse</span><br>
+            <span class="description"> 
+            toffee, feulletine, dark chocolate, dulce de leche sponge cake, orange meringue, coconut crumble, currants<br>
+              <span class="price">14</span><br>
+            <span class="allergies">NUTS, GLUTEN, DAIRY</span></span>  
+
+`;
