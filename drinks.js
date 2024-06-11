189f27691fed1a61eb5b192e8eaf20d299f1a4a8
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:08:42 -0400

    create drinks.js

---
+++ drinks.js
@@ -0,0 +1,119 @@
+let nathan = `
+<span class="name">The Nathan II,</span>
+<span class="description"> 
+High Proof Bourbon, Lustau Sweet Vermut, 
+Marseille Amaro, Aromatic bitters / 
+<strong>18</strong>
+</span>`;
+
+let blackForest = `<span class="name">Black Forest,</span><span class="description"> Monkey 47 Gin, St~Germain, fresh lemon juice, blackberries, rosemary, egg white / <strong>16</strong></span>`;
+
+let mezquilaMelon = `
+<span class="name">Mezquila Melón,</span>
+<span class="description"> 
+Fortaleza Blanco Tequila, 
+Giffard Pamplemousse, 
+El Buho Mezcal, 
+honeydew melon, 
+fresh lime juice / 
+<strong>16</strong>
+</span>`;
+
+let verde = `
+<span class="name">Verde,</span>
+<span class="description"> 
+jalape&ntilde;o infused Lunazul Blanco Tequila, 
+Clément Créole Shrubb, agave nectar, 
+fresh lemon juice, kiwi / 
+<strong>16</strong>
+</span>`;
+
+let lastWord = `
+<span class="name">Last Word,</span>
+<span class="description"> 
+Green Chartreuse, Luxardo Maraschino, 
+Roku Gin, fresh lime juice, 
+fennel & sugar rim / 
+<strong>19</strong>
+</span>`;
+
+let smokedManhattan = `
+<span class="name">Smoked Manhattan,</span>
+<span class="description"> 
+Michter's Single Barrel Rye Whiskey, Lustau Vermut Rojo, 
+Cardamaro, spiced cherry bitters / 
+<strong>19</strong>
+</span>`;
+
+let paperPlane = `<span class="name">Paper Plane,</span><span class="description"> Amaro Nonino, Aperol, Michter's Small Batch Bourbon, fresh lemon juice / <strong>14</strong></span>`;
+
+let autumnOrchard = `<span class="name">Autumn Orchard,</span><span class="description"> Four Roses Bourbon, Apple Cider, St. Elizabeth Allspice Dram, cinnamon syrup, fresh lemon juice / <strong>14</strong></span>`;
+
+let losSieteM = `<span class="name">Los Siete M,</span><span class="description"> Los Siete Misterios Mezcal, pineapple, fresh lemon juice, cocoa bitters / <strong>14</strong></span>`;
+
+let velvet = `
+<span class="name">The Velvet,</span>
+<span class="description"> 
+El Buho Mezcal, Aperol, Velvet Falernum, 
+fresh lime juice / 
+<strong>16</strong>
+</span>`;
+
+let autumnSpirits = `
+<span class="name">Autumn Spirits,</span>
+<span class="description">
+ Teramana Tequila, fresh lime juice, Ancho Reyes, Aperol, bell pepper
+ / <strong>16</strong></span>`;
+
+let vodkaTropical = `
+<span class="name">Vodka Tropical,</span>
+<span class="description"> 
+Haku Vodka, Pineapple Shrubb, Yellow Chartreuse, 
+egg white, aromatic bitters / 
+<strong>17</strong>
+</span>`;
+
+let misteriosChipotle = `
+<span class="name">Los Misterios del Chipotle,</span>
+<span class="description"> 
+Los Siete Misterios Mezcal, Liquore Strega, Fino Sherry, 
+chipotle, sage, fresh lemon juice / 
+<strong>16</strong>
+</span>`;
+
+let veranoSanlucar = `<span class="name">Verano en Sanl&uacute;car,</span><span class="description"> Manzanilla Sherry, Roku Gin, raspberry syrup, fresh lemon juice, egg white / <strong>15</strong></span>`;
+
+let cavaGimlet = `
+<span class="name">Cava Gimlet,</span>
+<span class="description"> 
+Cava, Gunpowder Irish Gin, fresh lime juice, 
+simple syrup / 
+<strong>16</strong>
+</span>`;
+
+let rosada = `<span class="name">Rosada,</span><span class="description"> Ros&eacute; wine, Grand Marnier, St~Germain, fresh fruits (glass/pitcher) / <strong>14</strong> / <strong>49</strong></span>`;
+
+let blanca = `<span class="name">Blanca,</span><span class="description"> Albari&ntilde;o white wine, Grand Marnier, St~Germain, fresh fruits (glass/pitcher) / <strong>14</strong> / <strong>49</strong></span>`;
+
+let tinta = `<span class="name">Tinta,</span><span class="description"> Rioja red wine, brandy de Jerez, Cointreau, fresh fruits (glass/pitcher) / <strong>14</strong> / <strong>49</strong></span>`;
+
+let kiwiSoda = `
+<span class="name">Kiwi Soda,</span>
+<span class="description"> 
+kiwi, fresh lime juice, sparkling water, agave nectar / 
+<strong>8</strong>
+</span>`;
+
+let paloma = `
+<span class="name">Paloma,</span>
+<span class="description"> 
+fresh grapefruit juice, fresh lemon juice, simple syrup / 
+<strong>8</strong>
+</span>`;
+
+let redGingerFizz = `
+<span class="name">Red Ginger Fizz,</span>
+<span class="description"> 
+strawberries, fresh lime, ginger beer, mint / 
+<strong>9</strong>
+</span>`;
