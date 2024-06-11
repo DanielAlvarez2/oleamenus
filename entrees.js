28b31a5bd58f56bf679813e0505f4456ea3216f3
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:10:07 -0400

    create entrees.js

---
+++ entrees.js
@@ -0,0 +1,46 @@
+let vegetables = `<span class="name">vegetariano</span><br><span class="description"><em>vegetarian platter;</em> polenta, eggplant, sweet piquillo pepper, mushrooms, french green beans, brussels sprouts, yellow squash, carrots, tomato, lotus root, romesco, cilantro raita <strong>29</strong><br><span class="allergies">nuts, garlic, dairy<br>can be made vegan</span></span>`;
+
+let scallops = 
+    `<span class="name">vieiras</span><br>
+    <span class="description">
+    <em>seared wild caught japanese sea scallops;</em>
+    cauliflower, potato, aj&iacute; amarillo, puffed quinoa, 
+    bell pepper-mango vinaigrette 
+    <strong>35</strong><br>
+    <span class="allergies">SHELLFISH, garlic, dairy
+    </span>
+    </span>`;
+
+let bacalao = `<span class="name">bacalao</span><br><span class="description"><em>fresh atlantic cod fish gratin with a&iuml;oli;</em> haricots verts, potatoes, cauliflower crudit&eacute;, tomato, red pepper nage, a&iuml;oli, cod fish mousse stick <strong>34</strong><br><span class="allergies">GARLIC, gluten, dairy</span></span>`;
+
+let fish = `
+<span class="name">lubina mediterránea</span><br>
+<span class="description">
+<em>mediterranean sea bass a la plancha;</em><br> 
+(skin-on filet), 
+mussels, clams, shrimp, potato, green peas, 
+judiones de la granja white beans, tomato, scallions, 
+spanish salsa verde 
+<strong>m/p</strong><br>
+<span class="allergies">GARLIC, gluten, shellfish</span>
+</span>`;
+
+let hen = `<span class="name">cornish hen</span><br><span class="description"><em>whole roasted small chicken;</em> potato confit, pickled organic brown beech mushrooms, tomato confit, garlic-lemon sauce <strong>32</strong><br><span class="allergies">garlic</span></span>`;
+let duck = `<span class="name">pato confitado</span><br><span class="description"><em>bella bella duck leg confit;</em> parsnip pur&eacute;e, zucchini, green apple, apple paste, brioche, fennel, lavender, citrus gastrique <strong>32</strong><br><span class="allergies">gluten, dairy</span></span>`;
+let cochinillo = `<span class="name">cochinillo</span><br><span class="description"><em>roasted boneless suckling pig;</em> lemon, thyme, honey-port wine, parmesan cheese dauphinoise potato, petit mesclun salad <strong>42</strong><br><em>(please allow 40 minutes cooking time)</em><br><span class="allergies">GARLIC, dairy</span></span>`;
+let solomillo = `<span class="name">solomillo</span><br><span class="description"><em>8 oz. grilled beef tenderloin;</em> glazed carrots, sunchokes, asparagus, port wine-chimichurri sauce <strong>45</strong><br><span class="allergies">GARLIC</span></span>`;
+
+let lobster = `<span class="name">lobster ⋅ clams</span><br>
+<span class="description">
+1-1/4 lbs Maine lobster off the shell, Manila clams, fingerling potatoes, celery, carrots, cauliflower, light seafood broth, lobster oil 
+<strong>47</strong><br>
+<span class="allergies">DAIRY SHELLFISH GARLIC BACON</span></span>`;
+
+let salmon = `
+<span class="name">salm&oacute;n</span><br>
+<span class="description">
+grilled atlantic salmon; sweet potato, carrots, soybeans, 
+poppy seeds,
+lemon confit vinaigrette, balsamic, basil
+<strong>32</strong><br>
+<span class="allergies"></span></span>`;
