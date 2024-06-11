3eb29bcd7222b57bf00c73dacce76e2a76d33f84
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:11:46 -0400

    create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+
+let legal = `consumer advisory: consumption of undercooked meat, poultry, eggs, or seafood may increase the risk of food-borne illnesses<br>please alert your server if you have special dietary requirements<br>all menu items are subject to change according to seasonality and availability`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
