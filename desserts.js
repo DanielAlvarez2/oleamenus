8484c992209a8088e6c6982e8cb5c97e4b5adb67
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:05:44 -0400

    create desserts.js

---
+++ desserts.js
@@ -0,0 +1,43 @@
+let meringue = `<span class='name'>upside down meringue</span> lime meringue, vanilla mascarpone ganache, pastry cream, aged sherry vinegar infused syrup, strawberries, hazelnut brittle`;
+
+let cheese = `<span class="name">cheese platter</span><span class="description"> for one <strong>12</strong> / for two <strong>18</strong><br><span class="allergies">DAIRY, gluten, nuts</span></span>`;
+
+let torrija = `<span class="name">torrija</span><span class="description"> caramelized soaked brioche, tahitian vanilla infused milk, honey-rosemary ice cream <strong>13</strong><br><span class="allergies">DAIRY, GLUTEN</span></span>`;
+
+let tropical = `<span class="name">tropical</span><span class="description"> almond-coconut sponge cake, milk chocolate, kalamansi-white chocolate cremoso, mango-pineapple gelatin, lime meringue &#x2022; pineapple curd &#x2022; coconut crumble &#x2022; coconut ice cream <strong>14</strong><br><span class="allergies">DAIRY, NUTS</span></span>`;
+
+let cannoliMisu = `<span class="name">deconstructed cannoli-misu</span><span class="description"> cocoa soil, mascarpone mousse, t&iacute;a mar&iacute;a gelatin, ladyfinger powder, coffee sauce <strong>14</strong><br><span class="allergies">DAIRY, GLUTEN</span></span>`;
+
+let chocolateTextures = `
+<span class="name">chocolate textures</span>
+<span class="description"> chocolate-mousse-vanilla ice cream sandwich &#x2022; 
+milk chocolate crispy rice, orange zest &#x2022; nougatine &#x2022; chocolate-hazelnut 
+ice cream &#x2022; rosemary-mint infused white chocolate soup(contains alcohol) 
+<strong>14</strong><br>
+<span class="allergies">DAIRY, GLUTEN, nuts</span></span>`;
+
+let veganChocolateMousse = `<span class="name">vegan chocolate mousse</span><span class="description"> 70% belgian chocolate, orange sponge cake, hazelnuts, orange zest, fresh berries, cherry syrup, sesame seed crunch <strong>13</strong><br><span class="allergies">nuts</span></span>`;
+
+let iceCream = `<span class="name">house-made ice cream</span><span class="description"> honey-rosemary, passion fruit, lemon peel, hazelnut-chocolate, cr&egrave;me br&ucirc;l&eacute;e <strong>three scoops 12</strong><br><span class="allergies">DAIRY</span></span>`;
+
+let sorbet = `<span class="name">house-made sorbet</span><span class="description"> strawberry sorbet, <span class="">blueberry sorbet</span> <strong>three scoops 12</strong></span>`;
+
+let lemonPie = `
+<span class="name">modern lemon pie</span>
+<span class="description"> meringue, pineapple ice cream, lemon curd</span> 
+<strong>14</strong></span><br>
+<span class="allergies">DAIRY, GLUTEN</span>`;
+
+let strawberriesCream = `
+<span class="name">strawberries and cream</span>
+<span class="description"> tahitian vanilla panna cotta, strawberry compote,
+ orange sponge cake, pistachio tuile, white balsamic air</span> 
+<strong>14</strong></span><br>
+<span class="allergies">DAIRY, GLUTEN, nuts</span>`;
+
+
+let sobaito = `<span class="name">sobaito</span><br><span class="description">
+white chocolate sponge cake, white chocolate-vanilla ganache, 
+mango-ginger compote, kiwi gelatin, chocolate caramel tuile, 
+Thai basil-black pepper mocktail
+ </span> <strong>14</strong></span><br><span class="allergies">DAIRY, GLUTEN</span>`;
