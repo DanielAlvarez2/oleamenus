021f71324a9497a5ddce8eaa9aabce45edddf290
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-10 21:03:37 -0400

    create appetizers.js

---
+++ appetizers.js
@@ -0,0 +1,64 @@
+
+
+
+let jamon = `
+<span class="name">
+jam&oacute;n ib&eacute;rico
+</span> 
+<span class="description">
+1oz. <strong>19</strong> / 2oz. <strong>38</strong></span>`;
+
+let charcuterie = `
+<span class="name">charcuter&iacute;a</span><br>
+<span class="description">
+lomo, chorizo and salchichón ib&eacute;rico, manchego cheese, 
+house made pickled vegetables, 
+chicken liver spread-port reduction, walnuts, toasted bread 
+<strong>22</strong><br>
+<span class="allergies">
+nuts, gluten, dairy</span></span>`;
+
+let kaleSalad = `
+<span class="name">kale salad</span><br>
+<span class="description">
+baby kale, arugula, pear, dried cranberry, candied walnut,
+lemon zest, Parmesan cheese, white balsamic vinaigrette
+<strong>14</strong><br>
+<span class="allergies">nuts, dairy, can be made vegan</span></span>`;
+
+let salad = `<span class="name">ensalada de temporada</span><br><span class="description"><em>organic seasonal salad;</em> lettuce, granny smith apples, grape tomatoes, red onions, honey-dijon dressing <strong>14</strong><br><span class="allergies">can be made vegan</span></span>`;
+
+let gazpacho = `<span class="name">gazpacho</span><br><span class="description"><em>cold vegetable soup;</em> tomatoes, cucumber, bell pepper, extra virgin olive oil, aged sherry vinegar, bread tuile, marcona almond cloud <strong>15</strong><br><span class="allergies">GARLIC, nuts, gluten, dairy<br>can be made vegan</span></span>`;
+let ceviche = 
+`<span class="name">ceviche mixto</span>
+<span class="description">
+<strong> (mexican style)</strong><br>
+<em>seafood ceviche;</em> 
+bay scallops, mussels, shrimp, octopus, red onion, cilantro, 
+tomato, mango, citrus, aj&iacute; amarillo air 
+<strong>16</strong><br>
+<span class="allergies">SHELLFISH, GARLIC</span></span>`;
+
+let tartare = `<span class="name">tartar de at&uacute;n</span><br><span class="description"><em>yellowfin tuna tartare;</em> tomato, ginger, soy sauce, sesame seeds, a&iuml;oli, crispy wonton <strong>18</strong><br><span class="allergies">gluten/SOY, garlic</span></span>`;
+
+let pulpo = 
+`<span class="name">pulpo</span><br>
+<span class="description">
+<em>spanish octopus a la plancha;</em>
+potato foam, celery, sweet piquillo pepper, tomato, 
+capers, paprika 
+<strong>17</strong><br>
+<span class="allergies">
+SHELLFISH, GARLIC, DAIRY</span></span>`;
+
+let chipirones = 
+`<span class="name">chipirones</span><br>
+<span class="description">
+<em>point judith baby squid ragout;</em> 
+squid ink, albari&ntilde;o wine, calasparra rice 
+<strong>17</strong><br>
+<span class="allergies">
+SHELLFISH, GARLIC, GLUTEN, DAIRY</span></span>`;
+
+let croquettes = `<span class="name">croquetas de rabo</span><br><span class="description"><em>oxtail beef-wild mushroom croquettes;</em> 3 pieces, port wine-oxtail jus, caramelized onions, dijon mustard <strong>15</strong><br><span class="allergies">GARLIC, GLUTEN, DAIRY</span></span>`;
+let foie = `<span class="name">foie gras</span><br><span class="description"><em>2.5 oz. grade A bella bella foie gras;</em> polenta, orange, blueberry pur&eacute;e, yuca chips, px reduction <strong>22</strong><br><span class="allergies">dairy</span></span>`;
